0e2cfb0ce68e9f46619731627788f1b829bae934
karamakcsc <42406861+karamakcsc@users.noreply.github.com>
2020-09-20 00:10:31 +0300

    Update payment_entry.py

--- masar_cheques/custom/payment_entry/payment_entry.py
+++ masar_cheques/custom/payment_entry/payment_entry.py
@@ -3,5 +3,32 @@ import frappe, erpnext
 from frappe.utils import flt, cstr, nowdate, comma_and
 from frappe import throw, msgprint, _
 
-def click_test_button(self, method):
-	frappe.msgprint(" Hi My Name is Emad Khabour")
+def dfTest(self, method):
+#	frappe.msgprint(" Hi My Name is Emad Khabour")
+	doc = frappe.get_doc('Payment Entry','ACC-PAY-2020-00003')
+
+@frappe.whitelist()
+def dfModeOfPaymentCash():
+	frappe.db.set_value('Payment Entry', 'ACC-PAY-2020-00003', 'mode_of_payment', 'Cash')
+
+
+@frappe.whitelist()
+def defSendSMS():
+	frappe.msgprint("Hi ,Send SMS , I am Emad")
+
+@frappe.whitelist()
+def defAddTask():
+	doc = frappe.new_doc('Task')
+	doc.title = 'Khabour'
+	doc.subject = 'My name is Emad'
+	doc.insert()
+	# doc = frappe.get_doc({
+	#     'doctype': 'Task',
+	#     'title': 'Yasser Task',
+	#     'subject': 'Hello Dears',
+	# })
+	#doc.insert()
+
+@frappe.whitelist()
+def defTestButton():
+	frappe.msgprint("Hi ,Test Button")
